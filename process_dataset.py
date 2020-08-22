f9fc9c405b457884ced5eedff34b88a7aee8ab09
Chang Ee Chuan <changec@NTUCLINK.SG>
2020-08-22 18:07:41 +0800

    Added logic to process splitting of name into first and last

--- process_dataset.py
+++ process_dataset.py
@@ -8,6 +8,40 @@ abb_set = {
 	'Mr.','Miss','Mrs.','Jr.','MD','DDS','PhD','DVM','Dr.','Ms.','III','II','IV'
 }
 
+#opening daily dataset file and begin processing
+with open('dataset.csv', newline='') as f:
+
+	#reader is a iterator which can be looped to process line by line
+	reader = csv.reader(f)
+
+	#Skip header row
+	header = next(reader)
+
+	#process each row in reader iterator
+	for row in reader:
+
+		#setting variables accordingly
+		name = row[0]
+		price = row[1]
+		above_100 = "false"
+
+		#split name string into a list, delimited by spaces
+		name = name.split(" ")
+
+		#declaring variables for first and last name
+		first_name = ""
+		last_name = ""
+
+		#checking each component in name list
+		for name_component in name:
+			#check if the component is not an abbreviation, which means it is a proper name component
+			if name_component not in abb_set:
+				#check if first name have been loaded
+				if first_name == "":
+					first_name = name_component
+				#check if last name have been loaded
+				else:
+					last_name = name_component
 
 
 	
