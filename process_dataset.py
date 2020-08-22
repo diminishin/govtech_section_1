25100f551a2f6c219addec14e11aedfa603a9412
Chang Ee Chuan <changec@NTUCLINK.SG>
2020-08-22 18:06:26 +0800

    Declaring all the abbreviations found in name column

---
+++ process_dataset.py
@@ -0,0 +1,16 @@
+#Author: Chang Ee Chuan
+#Date: 2020-08-22
+
+import csv
+
+#contains all the abbreviations in name field
+abb_set = {
+	'Mr.','Miss','Mrs.','Jr.','MD','DDS','PhD','DVM','Dr.','Ms.','III','II','IV'
+}
+
+
+
+	
+
+
+
